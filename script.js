0bcbdca63872f06a805dead7ee3c060d118015cd
EkhsanFitri94 <ekhsanfitri123@gmail.com>
2025-09-22 10:33:22 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Bila page load
+console.log("Portfolio Ekhsan ready!");
+
+// Fungsi bila butang ditekan
+function showMessage() {
+    alert("Hello! Terima kasih kerana melawat portfolio saya 😊");
+}
+
+// Fungsi tukar tema (dark mode)
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+}
